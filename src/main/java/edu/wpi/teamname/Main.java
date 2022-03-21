98cf92a88d5f671448414946db8c45f3bb583dc5
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-03-21 12:54:26 -0400

    Git Exercise Intstructions

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,21 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Name");
+    // Step 4: Committing Changes
+    // TODO: Brandon !!!
+    // TODO: Brian !!!
+    // TODO: Grace !!!
+    // TODO: Jack !!!
+    // TODO: Matthew !!!
+    // TODO: Mia !!!
+    // TODO: Nelson !!!
+    System.out.println("Nick");
+    // TODO: Vagmi !!!
+    // TODO: Vishnu !!!
+
+    // Step 5: Branching
+    System.out.println("Branch – Name");
+
     App.launch(App.class, args);
   }
 }
